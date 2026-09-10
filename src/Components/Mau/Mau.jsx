d49184723f7cecb2aeb00178a922045f6db861e0
Ayush Kumar <153271606+Ayushkr926@users.noreply.github.com>
2026-09-11 04:00:02 +0530

    Fix GitHub Pagest

--- src/Components/Mau/Mau.jsx
+++ src/Components/Mau/Mau.jsx
@@ -3,7 +3,7 @@ import gsap from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 gsap.registerPlugin(ScrollTrigger);
 
-import styles from "./Mau.module.css";
+import styles from "src/Components/Mau/Mau.module.css";
 import image1 from "../../assets/maupic1.jpg";
 import image2 from "../../assets/maupic2.jpg";
 
